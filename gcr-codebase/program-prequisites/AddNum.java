ea2373b507ec553a68ad0559a453758f7012c9ce
Abhijeetsingh1608 <kumarabhisingh2020@gmail.com>
2025-12-17 12:58:37 +0530

    Delete folder program-prequisites

--- gcr-codebase/program-prequisites/AddNum.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class AddNum{
-public static void main(String[]args){
-Scanner sc=new Scanner(System.in);
-int a=sc.nextInt();
-int c =sc.nextInt();
-System.out.println(a+c);
-}
-}
--- gcr-codebase/program-prequisites/Bridgelabz.java
+++
@@ -1,6 +0,0 @@
-import java.util.*;
-class Bridzelabz{
-public static void main(String[] args){
-System.out.println("Welcome to Bridzelabz");
-}
-}
