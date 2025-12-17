4d257e800d6e6031bdb5399a6ae34fff45598ab8
Abhijeetsingh1608 <kumarabhisingh2020@gmail.com>
2025-12-17 16:14:48 +0530

    [Abhijeet]: Added Harry's Current age program

---
+++ gcr-codebase/programming-elements/levelone/HarryAge.java
@@ -0,0 +1,14 @@
+//Create HarryAge Class to compute age
+public class HarryAge{
+public static void main(String[]args){
+
+// Creating variable Birthyear and Currentyear
+int Birthyear=2000;
+int CurrentYear=2025;
+//Calculating the age by Subtracting the Birthyear from Currentyear
+int age=(CurrentYear-Birthyear);
+
+//Display the result
+System.out.println("Harry's age in 2025 is " + age);
+}
+}
