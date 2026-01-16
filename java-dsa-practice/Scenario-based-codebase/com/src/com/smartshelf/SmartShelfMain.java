baf1f3ee6ddbee47a789c975b40e6e61f7e847f4
Abhijeetsingh1608 <kumarabhisingh2020@gmail.com>
2026-01-16 14:19:45 +0530

    [Abhijeet]:Added program for smartshelf

---
+++ java-dsa-practice/Scenario-based-codebase/com/src/com/smartshelf/SmartShelfMain.java
@@ -0,0 +1,20 @@
+package com.smartshelf;
+
+
+public class SmartShelfMain {
+    public static void main(String[] args) {
+
+        Book[] books = {
+            new Book("Data Structures"),
+            new Book("Algorithms"),
+            new Book("Computer Networks"),
+            new Book("Operating Systems")
+        };
+
+        SmartShelf shelf = new SmartShelf();
+        shelf.sortBooks(books);
+        shelf.display(books);
+    }
+}
+
+
