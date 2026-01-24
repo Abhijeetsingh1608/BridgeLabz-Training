158be2a11f7317def7c2bba79b958d39eac58fb8
Abhijeetsingh1608 <kumarabhisingh2020@gmail.com>
2026-01-25 01:17:52 +0530

    [Abhijeet]:removed exceptions folder

--- java-collections-practice/gcr-codebase/javacollectionspractice/src/com/exceptions/banktransaction/InsufficientBalanceException.java
+++
@@ -1,8 +0,0 @@
-package com.exceptionhandling.banktranscation;
-
-public class InsufficientBalanceException extends Exception {
-
-    public InsufficientBalanceException(String message) {
-        super(message);
-    }
-}
