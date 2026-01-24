e23755b026f5594ad9a0b9609cd7431c1a37a0a2
Abhijeetsingh1608 <kumarabhisingh2020@gmail.com>
2026-01-24 11:23:58 +0530

    [Abhijeet]:Added program for university digital record

---
+++ java-dsa-practice/Scenario-based-codebase/com/src/com/binarysearchtree/Universitydigital/StudentNode.java
@@ -0,0 +1,14 @@
+package com.binarysearchtree.Universitydigital;
+
+public class StudentNode {
+    int rollNo;
+    String name;
+    StudentNode left, right;
+
+    public StudentNode(int rollNo, String name) {
+        this.rollNo = rollNo;
+        this.name = name;
+        this.left = null;
+        this.right = null;
+    }
+}
